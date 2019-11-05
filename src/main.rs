60aec16efeca0b2d4d2b187bc91bf14b1edfbde6
Mike Zueff <mike.zueff@gmail.com>
2019-11-05 04:43:24 +0300

    Remove VK API stub.

--- src/main.rs
+++ src/main.rs
@@ -14,23 +14,35 @@ use sms::*;
 fn main() {
     println!("SMS started.");
 
-    let vk_api_tokens = file_to_vec("config/private_vk_api_tokens").expect(SMS_IO_ERROR_COMMON);
+    let sms_current_vp_api_token = 0;
+    let sms_vk_api_tokens = file_to_vec("config/private_vk_api_tokens").expect(SMS_IO_ERROR_COMMON);
 
     println!(
         "VK API tokens:{}",
-        vk_api_tokens
+        sms_vk_api_tokens
             .iter()
-            .fold(String::new(), |acc, token| acc + "\n" + &token.to_string())
+            .fold(String::new(), |acc, token| acc
+                + "\n- "
+                + &token.to_string()
+                + ".")
     );
 
-    let conn = sms_db_conn_establish();
+    println!(
+        "Current API key:\n{}.",
+        sms_vk_api_tokens[sms_current_vp_api_token]
+    );
+
+    let sms_db_connection = sms_db_conn_establish();
 
     let result: i64 = vor_obl_settlements
         .count()
-        .get_result(&conn)
+        .get_result(&sms_db_connection)
         .expect(SMS_DB_ERROR_QUERY);
 
-    println!("Settlements count in the database: {}.", result);
+    println!(
+        "Voronezh Oblast settlements count in the database: {}.",
+        result
+    );
 
     println!("SMS stopped.");
 }
--- vk_api/src/lib.rs
+++
@@ -1,8 +0,0 @@
-#[cfg(test)]
-mod tests {
-/* TODO */
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
