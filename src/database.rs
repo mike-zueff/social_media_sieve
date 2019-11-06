eb0271dfdae7a407c0a291ed437b70a374eea59a
Mike Zueff <mike.zueff@gmail.com>
2019-11-06 20:45:58 +0300

    Move some code from main.rs

--- src/database.rs
+++ src/database.rs
@@ -1,3 +1,4 @@
+use crate::schema::vor_obl_settlements::dsl::*;
 use diesel::prelude::*;
 
 const SMS_DB_PATH: &str = "database/sms_db_sqlite";
@@ -6,6 +7,25 @@ pub const SMS_DB_ERROR_QUERY: &str = "Database query error occured.";
 
 /* Connect to the database. */
 pub fn sms_db_conn_establish() -> SqliteConnection {
-    SqliteConnection::establish(SMS_DB_PATH)
-        .unwrap_or_else(|_| panic!("Can't connect to the database."))
+    SqliteConnection::establish(SMS_DB_PATH).expect("Can't connect to the database.")
+}
+
+/* Check and fetch settlements. */
+pub fn sms_db_settl_fetch(conn: &SqliteConnection) {
+    let result: i64 = vor_obl_settlements
+        .count()
+        .get_result(conn)
+        .expect(SMS_DB_ERROR_QUERY);
+
+    if result == 0 {
+        println!(
+            "Number of Voronezh Oblast settlements in the database: {}.",
+            result
+        );
+    } else {
+        println!(
+            "Number of Voronezh Oblast settlements in the database: {}.",
+            result
+        );
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,35 +2,16 @@
 extern crate diesel;
 
 mod database;
-use database::*;
-
 mod schema;
-use schema::vor_obl_settlements::dsl::*;
 
-use diesel::prelude::*;
+use database::*;
 
 /* SMS entry point. */
 fn main() {
     println!("SMS started.");
 
     let sms_db_conn = sms_db_conn_establish();
-
-    let result: i64 = vor_obl_settlements
-        .count()
-        .get_result(&sms_db_conn)
-        .expect(SMS_DB_ERROR_QUERY);
-
-    if result == 0 {
-        println!(
-            "Number of Voronezh Oblast settlements in the database: {}.",
-            result
-        );
-    } else {
-        println!(
-            "Number of Voronezh Oblast settlements in the database: {}.",
-            result
-        );
-    }
+    sms_db_settl_fetch(&sms_db_conn);
 
     println!("SMS stopped.");
 }
