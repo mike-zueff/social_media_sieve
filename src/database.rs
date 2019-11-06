3941719259c64cbf63a6d5a08299e0962b29a243
Mike Zueff <mike.zueff@gmail.com>
2019-11-06 19:04:45 +0300

    Update database.rs

--- src/database.rs
+++ src/database.rs
@@ -1,7 +1,7 @@
 use diesel::prelude::*;
 use sms::*;
 
-/* Database connection. */
+/* Connect to the database. */
 pub fn sms_db_conn_establish() -> SqliteConnection {
     SqliteConnection::establish(SMS_DB_PATH)
         .unwrap_or_else(|_| panic!("Can't connect to the database."))
